245608e174e2949c58b8b48cf5ae5998ab29e3ff
Amanda Borim <amandafborim@gmail.com>
2021-06-17 14:36:53 -0300

    componentes update e delete do CRUD

--- Angular9/crud-cod3r/frontend/src/app/app-routing.module.ts
+++ Angular9/crud-cod3r/frontend/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -12,6 +14,12 @@ const routes: Routes = [{
 }, {
     path: "products/create",
     component: ProductCreateComponent
+}, {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+}, {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
 }];
 
 @NgModule({
